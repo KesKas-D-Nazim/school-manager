e39ec5ad896113bbb8cdc24b9c6d9d8f019649d5
oussamatabni <0s4ma.t8n@gmail.com>
2026-04-23 03:02:47 +0100

    updated student getCourse to get only enrolled courses along side with the teacher name

--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -1,18 +1,33 @@
 import { db } from "../../db/db";
-import { coursesTable, notificationsTable, studentsTable } from "../../db/schemas";
+import { coursesTable, notificationsTable, studentsTable, enrollmentsTable, teachersTable, users } from "../../db/schemas";
 import { eq } from "drizzle-orm";
 
 
 export const getCourses = async (c: any) => {
   try {
     const user = c.get("user");
-    console.log("USER:", JSON.stringify(user, null, 2)); 
+    // console.log("USER:", JSON.stringify(user, null, 2)); 
+    const studentId = user.info.id;
 
     const courses = await db
-      .select()
-      .from(coursesTable)
-      .where(eq(coursesTable.schoolId, user.info.schoolId)
-  );
+      .select({
+        id: coursesTable.id,
+        name: coursesTable.name,
+        description: coursesTable.description,
+        teacherName: users.name,
+        createdAt: coursesTable.createdAt,
+      })
+      .from(enrollmentsTable)
+      .innerJoin(coursesTable, eq(enrollmentsTable.courseId, coursesTable.id))
+      .leftJoin(teachersTable, eq(coursesTable.teacherId, teachersTable.id))
+      .leftJoin(users, eq(teachersTable.userId, users.id))      
+      .where(eq(enrollmentsTable.studentId, studentId));    
+
+  //   const courses = await db
+  //     .select()
+  //     .from(coursesTable)
+  //     .where(eq(coursesTable.schoolId, user.info.schoolId)
+  // );
 
     return c.json({ courses });
   } catch (err) {
