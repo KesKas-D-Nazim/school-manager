8ccfd809dd69117d5c5db696b0182b17d5c50f13
nazim keskas <nazimkeskas51@gmail.com>
2026-05-01 18:26:12 +0100

    Added  Neon yml

--- src/modules/students/students.types.ts
+++ src/modules/students/students.types.ts
@@ -1,5 +1,4 @@
 import type { InferSelectModel } from "drizzle-orm";
-
 import { studentsTable, users } from "../../db/schemas";
 
 export type StudentWithUser = InferSelectModel<typeof studentsTable> & {
